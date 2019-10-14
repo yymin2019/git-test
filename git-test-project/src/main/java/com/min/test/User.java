d3213016e13dc1e50e4acf9deb62d5725d931598
YT <730278353@qq.com>
2019-10-14 21:14:59 +0800

    添加git本地仓库

---
+++ git-test-project/src/main/java/com/min/test/User.java
@@ -0,0 +1,12 @@
+package com.min.test;
+
+/**
+ * com.min.test User
+ *
+ * @author yymin
+ * @dateTime 2019/10/14 0014 下午 9:10
+ */
+public class User {
+    private Integer id;
+    private String name;
+}
